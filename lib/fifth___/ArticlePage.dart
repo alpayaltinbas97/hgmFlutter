0c392f2fe1261e42e6cca987ba9c007892b5a9d6
Alpay ALTINBAŞ <alpay.altinbas@hrtserver.local>
2022-06-15 09:33:02 +0300

    optimisation

--- lib/fifth___/ArticlePage.dart
+++ lib/fifth___/ArticlePage.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 
 /*
 third_screen.dart içerisindeki pdf görüntüleyici ve json verilerini aldığımız yöntemin neredeyse aynısı burada da mevcut.
@@ -250,7 +250,9 @@ class UserTile extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => pdfOpener(a:user.name + "-" + user.id + ".pdf",),
+              builder: (context) => PDFViewerCachedFromUrl(
+                url: 'https://www.harita.gov.tr/uploads/files/articles/'+ user.name + "-" + user.id + ".pdf",
+              ),
             ),
           );
         },
@@ -325,26 +327,135 @@ class UserTile extends StatelessWidget {
   }
 }
 
+class PDFViewerCachedFromUrl extends StatelessWidget {
+  PDFViewerCachedFromUrl({Key? key, required this.url}) : super(key: key);
 
-class pdfOpener extends StatefulWidget {
-  String a;
-  pdfOpener({Key? key,required this.a}) : super(key: key);
-
-  @override
-  _PdfOpener createState() => _PdfOpener();
-}
-
-class _PdfOpener extends State<pdfOpener> {
+  final String url;
+  final Completer<PDFViewController> _pdfViewController =
+  Completer<PDFViewController>();
+  final StreamController<String> _pageCountController =
+  StreamController<String>();
+  final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Makaleler"),
+        title: const Text('Makaleler'),
         backgroundColor: Colors.grey,
+        actions: <Widget>[
+          StreamBuilder<String>(
+              stream: _pageCountController.stream,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  return Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.redAccent,
+                      ),
+                      child: Text(snapshot.data!),
+                    ),
+                  );
+                }
+                return const SizedBox();
+              }),
+        ],
       ),
-      body: SfPdfViewer.network(
-        'https://www.harita.gov.tr/uploads/files/articles/' + widget.a,
+      body: PDF(
+        enableSwipe: true,
+        autoSpacing: true,
+        pageFling: true,
+        onPageChanged: (int? current, int? total) =>
+            _pageCountController.add('${current! + 1} - $total'),
+        onViewCreated: (PDFViewController pdfViewController) async {
+          _pdfViewController.complete(pdfViewController);
+          final int currentPage = await pdfViewController.getCurrentPage() ?? 0;
+          final int? pageCount = await pdfViewController.getPageCount();
+          _pageCountController.add('${currentPage + 1} - $pageCount');
+        },
+      ).fromUrl(
+        url,
+        placeholder: (double progress) => Center(child: Text('% $progress',style: TextStyle(
+            color: Colors.redAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 30))),
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),
+      ),
+      floatingActionButton: FutureBuilder<PDFViewController>(
+        future: _pdfViewController.future,
+        builder: (_, AsyncSnapshot<PDFViewController> snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  child: const Text('Sayfa'),
+                  backgroundColor: Colors.redAccent,
+                  onPressed: () async {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () => Navigator.pop(context, true), // passing true
+                              child: Icon(FontAwesomeIcons.angleRight),
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor: Colors.redAccent, // Background Color
+                              ),
+                            ),
+                          ],
+                          title: Text('Sayfa Numarası Giriniz'),
+                          content: TextField(
+                            controller: myController,
+                            decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.redAccent),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.redAccent),
+                              ),
+                              hintText: '...',
+                              suffixIcon: IconButton(
+                                onPressed:
+                                  myController.clear,
+                                icon: Icon(Icons.clear),
+                                color: Colors.redAccent,
+                                splashColor: Colors.redAccent,
+                              ),
+                            ),
+                            onChanged: (value) async{
+                              final PDFViewController pdfController = snapshot.data!;
+                              var one = int.parse(myController.text);
+                              final int currentPage = one;
+                              final int? pageCount = await pdfController.getPageCount();
+                              var pageCounter = int.parse(pageCount.toString());
+                              if (currentPage >= 0 && currentPage<pageCounter) {
+                                pdfController.setPage(currentPage-1);
+                              }
+                              else {
+                                await ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                  behavior: SnackBarBehavior.floating,
+                                  backgroundColor: Colors.redAccent,
+                                  content: Text("Lütfen Geçerli Bir Değer Giriniz"),
+                                ));
+                              }
+                              },
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 /*
 import kısmında görebileceğimiz pdfviewer kütüphanesi ile bir widget oluşturarak,
@@ -12,7 +15,6 @@ void main() {
     home: SecondScreen(),
   ));
 }
-final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
 
 /// Represents Homepage for Navigation
 class SecondScreen extends StatefulWidget {
@@ -26,13 +28,143 @@ class _SecondScreen extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Son Sayı',style: TextStyle(fontSize: 18,),),
-        backgroundColor: Colors.grey,
+      body:
+      PDFViewerCachedFromUrl(
+        url: 'https://www.harita.gov.tr/uploads/files/mapmagazines/sonsayi.pdf',
       ),
-      body: SfPdfViewer.network(
-        'https://www.harita.gov.tr/uploads/files/mapmagazines/sonsayi.pdf',
-        key: _pdfViewerKey,
+    );
+  }
+}
+
+class PDFViewerCachedFromUrl extends StatelessWidget {
+  PDFViewerCachedFromUrl({Key? key, required this.url}) : super(key: key);
+
+  final String url;
+  final Completer<PDFViewController> _pdfViewController =
+  Completer<PDFViewController>();
+  final StreamController<String> _pageCountController =
+  StreamController<String>();
+  final myController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Son Sayı'),
+        backgroundColor: Colors.grey,
+        actions: <Widget>[
+          StreamBuilder<String>(
+              stream: _pageCountController.stream,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  return Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.redAccent,
+                      ),
+                      child: Text(snapshot.data!),
+                    ),
+                  );
+                }
+                return const SizedBox();
+              }),
+        ],
+      ),
+      body: PDF(
+        enableSwipe: true,
+        autoSpacing: true,
+        pageFling: true,
+        onPageChanged: (int? current, int? total) =>
+            _pageCountController.add('${current! + 1} - $total'),
+        onViewCreated: (PDFViewController pdfViewController) async {
+          _pdfViewController.complete(pdfViewController);
+          final int currentPage = await pdfViewController.getCurrentPage() ?? 0;
+          final int? pageCount = await pdfViewController.getPageCount();
+          _pageCountController.add('${currentPage + 1} - $pageCount');
+        },
+      ).fromUrl(
+        url,
+        placeholder: (double progress) => Center(child: Text('% $progress',style: TextStyle(
+            color: Colors.redAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 30))),
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),
+      ),
+      floatingActionButton: FutureBuilder<PDFViewController>(
+        future: _pdfViewController.future,
+        builder: (_, AsyncSnapshot<PDFViewController> snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  child: const Text('Sayfa'),
+                  backgroundColor: Colors.redAccent,
+                  onPressed: () async {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () => Navigator.pop(context, true), // passing true
+                              child: Icon(FontAwesomeIcons.angleRight),
+                              style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor: Colors.redAccent, // Background Color
+                              ),
+                            ),
+                          ],
+                          title: Text('Sayfa Numarası Giriniz'),
+                          content: TextField(
+                            controller: myController,
+                            decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.redAccent),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.redAccent),
+                              ),
+                              hintText: '...',
+                              suffixIcon: IconButton(
+                                onPressed:
+                                myController.clear,
+                                icon: Icon(Icons.clear),
+                                color: Colors.redAccent,
+                                splashColor: Colors.redAccent,
+                              ),
+                            ),
+                            onChanged: (value) async{
+                              final PDFViewController pdfController = snapshot.data!;
+                              var one = int.parse(myController.text);
+                              final int currentPage = one;
+                              final int? pageCount = await pdfController.getPageCount();
+                              var pageCounter = int.parse(pageCount.toString());
+                              if (currentPage >= 0 && currentPage<pageCounter) {
+                                pdfController.setPage(currentPage-1);
+                              }
+                              else {
+                                await ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                  behavior: SnackBarBehavior.floating,
+                                  backgroundColor: Colors.redAccent,
+                                  content: Text("Lütfen Geçerli Bir Değer Giriniz"),
+                                ));
+                              }
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
